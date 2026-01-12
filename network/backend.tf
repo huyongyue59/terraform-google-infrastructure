97cc6e7edbfb36c24b897ec2eac97ae501fd8954
huyongyue <huyongyue>
2026-01-12 22:56:52 +0900

    Add VPC module with configuration, outputs, and metadata files

--- network/backend.tf
+++ network/backend.tf
@@ -3,4 +3,11 @@ terraform {
     bucket  = "your-unique-bucket-name"
     prefix  = var.feature_name
   }
+  required_version = ">= 1.3"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 6.19.0"
+    }
+  }
 }
